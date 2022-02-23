c60f4ae21f5b0e1388e4503f5dc081953cd59321
Ben Bangert <ben@groovie.org>
2022-02-22 18:43:26 -0800

    Remove old z files.

--- install-prezto.sh
+++ install-prezto.sh
@@ -6,7 +6,11 @@ git clone --recursive https://github.com/bbangert/prezto.git "${ZDOTDIR:-$HOME}/
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  mv "$rcfile" "$rcfile.bak"
+done
+
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done  
+done
 
 echo "Done"
