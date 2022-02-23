6c1ce79714d706bc191cc0d82c1609b3da4c9857
Ben Bangert <ben@groovie.org>
2022-02-22 17:57:16 -0800

    Update git and prezto setup.

---
+++ install-prezto.sh
@@ -0,0 +1,12 @@
+ #!/usr/bin/zsh
+
+echo "Installing prezto"
+
+git clone --recursive https://github.com/bbangert/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done  
+
+echo "Done"
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Install GNU stow / zsh
+if ! command -v stow >/dev/null @>&1; then
+    if command -v apt-get >/dev/null 2>&1; then
+        sudo apt-get install -y stow zsh
+    fi
+fi
+
+# Backup common existing files
+for f in .bashrc .bash_profile .bash_logout; do
+    if [ -e ~/$f ]; then
+        mv ~/$f ~/$f.bak
+    fi
+done
+
+# Stow dotfiles
+stow --target="$HOME" --stow git
+
+
+# Install prezto
+prezto_dir="$HOME/.zprezto"
+if [ -d $prezto_dir ]
+then
+  echo "Prezto already installed, exiting installation."
+  exit
+fi
+
+# Install prezto
+bash install-prezto.sh
