4b6e14c715ff2a2800143bdf56ef64dbcab31971
Ben Bangert <100193+bbangert@users.noreply.github.com>
2024-06-20 15:54:08 -0700

    shallow clone of prezto for perf

--- install-prezto.sh
+++ install-prezto.sh
@@ -2,7 +2,7 @@
 
 echo "Installing prezto"
 
-git clone --recursive https://github.com/bbangert/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive --shallow-submodules --depth 1 https://github.com/bbangert/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 # Remove existing zshrc
 rm -f ~/.zshrc ~/.zlogin ~/.zlogout ~/.zpreztorc ~/.zprofile  ~/.zshenv
